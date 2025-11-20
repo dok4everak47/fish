adc61953f399eef8fb1411739f9bacbedc860d4b
dok4ever <dok4ever@qq.com>
2025-11-21 01:31:53 +0800

    添加别名

--- config.fish
+++ config.fish
@@ -7,17 +7,19 @@ if status is-interactive
 				builtin cd -- "$cwd"
 			end
 			rm -f -- "$tmp"
-    	end
+    end
 
-    	set -gx EDITOR nvim
-			set -gx VISUAL nvim
+    set -gx EDITOR nvim
+	set -gx VISUAL nvim
 
-			# >>> mamba initialize >>>
-			# !! Contents within this block are managed by 'mamba shell init' !!
-			set -gx MAMBA_EXE "/home/dok4ever/miniforge3/bin/mamba"
-			set -gx MAMBA_ROOT_PREFIX "/home/dok4ever/miniforge3"
-			$MAMBA_EXE shell hook --shell fish --root-prefix $MAMBA_ROOT_PREFIX | source
-			# <<< mamba initialize <<<
+	# >>> mamba initialize >>>
+	# !! Contents within this block are managed by 'mamba shell init' !!
+	set -gx MAMBA_EXE "/home/dok4ever/miniforge3/bin/mamba"
+	set -gx MAMBA_ROOT_PREFIX "/home/dok4ever/miniforge3"
+	$MAMBA_EXE shell hook --shell fish --root-prefix $MAMBA_ROOT_PREFIX | source
+	# <<< mamba initialize <<<
+
+	abbr -a vi nvim
 
 end
 
