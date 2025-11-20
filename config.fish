bff4b515388cd7579f7dabf9771b37dc7cda9997
dok4ever <dok4ever@qq.com>
2025-11-21 01:29:39 +0800

    初始化

---
+++ config.fish
@@ -0,0 +1,24 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+    function y
+			set tmp (mktemp -t "yazi-cwd.XXXXXX")
+			yazi $argv --cwd-file="$tmp"
+			if read -z cwd < "$tmp"; and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+				builtin cd -- "$cwd"
+			end
+			rm -f -- "$tmp"
+    	end
+
+    	set -gx EDITOR nvim
+			set -gx VISUAL nvim
+
+			# >>> mamba initialize >>>
+			# !! Contents within this block are managed by 'mamba shell init' !!
+			set -gx MAMBA_EXE "/home/dok4ever/miniforge3/bin/mamba"
+			set -gx MAMBA_ROOT_PREFIX "/home/dok4ever/miniforge3"
+			$MAMBA_EXE shell hook --shell fish --root-prefix $MAMBA_ROOT_PREFIX | source
+			# <<< mamba initialize <<<
+
+end
+
+
