228c0a9748411969c08168a925c90d227ca6e361
dok4ever <65104429+dok4everak47@users.noreply.github.com>
2025-11-24 19:29:53 +0800

    Implement 'y' function for directory navigation
    Add function 'y' to change directory based on yazi output.

--- config.fish
+++ config.fish
@@ -35,6 +35,15 @@ if status is-interactive
 	end
 	# <<< conda initialize <<<
 
+	function y
+		set tmp (mktemp -t "yazi-cwd.XXXXXX")
+		yazi $argv --cwd-file="$tmp"
+		if read -z cwd < "$tmp"; and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+			builtin cd -- "$cwd"
+		end
+		rm -f -- "$tmp"
+	end
+
 end
 
 
